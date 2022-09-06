05c779d4c7acc0fd655f418d1e3ad1825363f193
Karye <karye@users.noreply.github.com>
2022-09-06 13:31:08 +0200

    Träna på XAML, events och namn

---
+++ WPF/Labb1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Labb1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Fånga upp klick på knappen
+        private void KlickRäknaUt(object sender, RoutedEventArgs e)
+        {
+            // Läsa av höjden
+            int höjd = int.Parse(rutaHöjd.Text);
+
+            // Läsa bredden
+            int bredd = int.Parse(rutaBredd.Text);
+
+            // Räkna ut arean
+            int area = bredd * höjd;
+
+            // Skriv ut i rutan resultat (TextBox)
+            //rutaResultat.Text = area.ToString();
+            rutaResultat.Text = $"{höjd} * {bredd} = {area}";
+        }
+    }
+}
