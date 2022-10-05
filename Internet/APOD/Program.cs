f32b1b237e2a1c4f24f385c6d7be5cc47e25d4dc
Karye <karye@users.noreply.github.com>
2022-10-05 11:12:59 +0200

    Tränat på Internet och avkoda JSON

---
+++ Internet/APOD/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Text.Json;
+
+namespace APOD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dagens bild från Nasa");
+
+            // Adressen till api-tjänsten
+            string url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+
+            // Steg 1: hämta
+            WebClient klient = new WebClient();
+            string json = klient.DownloadString(url);
+
+            // Steg 2: avkoda json-svaret
+            Apod apod = new Apod();
+            apod = JsonSerializer.Deserialize<Apod>(json);
+
+            // Skriv svaret
+            Console.WriteLine(apod.title);
+        }
+
+        // Matcha json
+        class Apod
+        {
+            public string explanation {get; set;}
+            public string title {get; set;}
+            public string url {get; set;}
+        }
+    }
+}
---
+++ Internet/ChuckNorris/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Text.Json;
+
+namespace ChuckNorris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hämta ett Chuck Norris skämt!");
+
+            // Adressen till api-tjänsten
+            string url = "https://api.chucknorris.io/jokes/random";
+
+            // Verktyg för att hämta data från Internet
+            WebClient klient = new WebClient();
+
+            // Utför anropet
+            string json = klient.DownloadString(url);
+
+            //Console.WriteLine(json);
+
+            // Avkoda JSON
+            // Skapa ett objekt 
+            Skämt skämtet = new Skämt();
+            skämtet = JsonSerializer.Deserialize<Skämt>(json);
+
+            // Skriv ut vad vi hittade
+            Console.WriteLine(skämtet.value);
+            Console.WriteLine(skämtet.icon_url);
+        }
+
+        // För att matcha innehållet i objektet json
+        class Skämt
+        {
+            public string icon_url {get; set;}
+            public string value {get; set;}
+        }
+    }
+}
