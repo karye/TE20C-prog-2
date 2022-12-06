a891d7018fc2c0c7fd2cd384ed8dd01c51ba5ba4
karye <karye.webb@gmail.com>
2022-12-06 14:30:53 +0100

    Fix

--- OOP/TamagotchiSpel/Tamagotchi.cs
+++ OOP/TamagotchiSpel/Tamagotchi.cs
@@ -1,4 +1,24 @@
+/* 
+Klassdiagram
+(+ betyder public, − betyder private)
+Tamagotchi
+− hunger: int
+− boredom: int
+− words: List<string>
+− isAlive: bool
+− generator: Random
++ name: string
++ Feed(): void
++ Hi(): void
++ Teach(word: string): void
++ Tick(): void
++ PrintStats(): void
++ GetAlive(): bool
+− ReduceBoredom(): void
+ */
+
 using System;
+using System.Collections.Generic;
 
 namespace TamagotchiSpel
 {
@@ -11,117 +31,74 @@ namespace TamagotchiSpel
         private bool isAlive;
         private Random generator;
 
-        // Detta fält är offentligt, så det kan användas från andra klasser
+        // Dessa fält är publika, så de kan användas utanför klassen
         public string name;
 
-        // Konstruktorn för Tamagotchi-klassen
+        // Konstruktor
         public Tamagotchi(string name)
         {
             this.name = name;
-
-            // Initiera fälten här
-            this.hunger = 0;
-            this.boredom = 0;
-            this.words = new List<string>();
-            this.isAlive = true;
-            this.generator = new Random();
+            hunger = 0;
+            boredom = 0;
+            words = new List<string>();
+            isAlive = true;
+            generator = new Random();
         }
 
-        // Detta är en offentlig metod för att mata Tamagotchien
+        // Metoder
         public void Feed()
         {
-            // Ändra hunger-fältet här
-            this.hunger = 0;
+            hunger--;
         }
 
-        // Detta är en offentlig metod för att hälsa på Tamagotchien
         public void Hi()
         {
-            // Skriv ut en hälsning här
-            Console.WriteLine("Hej, jag heter " + this.name);
-        }
-
-        // Detta är en offentlig metod för att lära Tamagotchien ett ord
-        public void Teach(string word)
-        {
-            // Lägg till ordet i listan med ord
-            this.words.Add(word);
-        }
-
-        // Detta är en offentlig metod för att uppdatera Tamagotchiens stats
-        public void Tick()
-        {
-            // Ändra hunger- och boredom-fälten här
-            this.hunger++;
-            this.boredom++;
-
-            if (this.hunger >= 10 || this.boredom >= 10)
-            {
-                // Ändra isAlive-fältet här
-                this.isAlive = false;
-            }
-
-            // Anropa ReduceBoredom-metoden här
+            Console.WriteLine("Hej, jag heter " + name);
+            boredom--;
             ReduceBoredom();
         }
 
-        // Detta är en offentlig metod för att skriva ut Tamagotchiens stats
+        public void Teach(string word)
+        {
+            words.Add(word);
+            boredom--;
+            ReduceBoredom();
+        }
+
+        public void Tick()
+        {
+            hunger++;
+            boredom++;
+            if (hunger > 10 || boredom > 10)
+            {
+                isAlive = false;
+            }
+        }
+
+
         public void PrintStats()
         {
-            // Skriv ut Tamagotchiens stats här
-            Console.WriteLine("Hunger: " + this.hunger);
-            Console.WriteLine("Boredom: " + this.boredom);
-            Console.WriteLine("Words: " + string.Join(", ", this.words));
-            Console.WriteLine("Is alive: " + this.isAlive);
+            Console.WriteLine("Hunger: " + hunger);
+            Console.WriteLine("Boredom: " + boredom);
+            Console.WriteLine("Words: " + string.Join(", ", words));
+            Console.WriteLine("Alive: " + isAlive);
         }
 
-        // Detta är en offentlig metod för att kolla om Tamagotchien är vid liv
         public bool GetAlive()
         {
-            // Returnera isAlive-fältet här
-            return this.isAlive;
+            return isAlive;
         }
 
-        // Detta är en privat metod för att minska Tamagotchiens trötthet
         private void ReduceBoredom()
         {
-            // Ändra boredom-fältet här
-            this.boredom = Math.Max(0, this.boredom - 1);
-        }
-
-        // Detta är en privat metod för att få Tamagotchien att tala
-        private void Speak()
-        {
-            // Skriv ut ett slumpat ord här
-            Console.WriteLine(this.words[this.generator.Next(this.words.Count)]);
-        }
-
-        // Detta är en privat metod för att få Tamagotchien att leka
-        private void Play()
-        {
-            // Ändra boredom-fältet här
-            this.boredom = 0;
-        }
-
-        // Detta är en privat metod för att få Tamagotchien att sova
-        private void Sleep()
-        {
-            // Ändra hunger-fältet här
-            this.hunger = Math.Max(0, this.hunger - 1);
-        }
-
-        // Detta är en privat metod för att få Tamagotchien att äta
-        private void Eat()
-        {
-            // Ändra hunger-fältet här
-            this.hunger = 0;
-        }
-
-        // Detta är en privat metod för att få Tamagotchien att dö
-        private void Die()
-        {
-            // Ändra isAlive-fältet här
-            this.isAlive = false;
+            if (boredom > 5)
+            {
+                int randomNumber = generator.Next(words.Count);
+                Console.WriteLine(name + " learned the word " + words[randomNumber] + "!");
+                words.RemoveAt(randomNumber);
+                boredom = 0;
+            }
         }
 
     }
+}
