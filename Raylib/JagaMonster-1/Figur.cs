5a684e58c0278739ba09e3396f687b9da424b33d
karye <karye.webb@gmail.com>
2023-01-10 13:31:05 +0100

    Skapat en klass för figurer

---
+++ Raylib/JagaMonster-1/Figur.cs
@@ -0,0 +1,27 @@
+using System;
+using Raylib_cs;
+
+namespace JagaMonster_1
+{
+    public class Figur
+    {
+        int _x = 100;
+        int _y = 100;
+        Texture2D _textur;
+
+        // Konstruktor
+        public Figur(string bild, int x, int y)
+        {
+            _textur = Raylib.LoadTexture("./resurser/" + bild);
+            _x = x;
+            _y = y;
+        }
+
+        // Rita ut metoden
+        public void Rita()
+        {
+            // Rita ut bilden
+            Raylib.DrawTexture(_textur, _x, _y, Color.WHITE);
+        }
+    }
+}
---
+++ Raylib/JagaMonster-1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Raylib_cs;
+
+namespace JagaMonster_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Raylib!");
+
+            // Skapa ett fönster
+            Raylib.InitWindow(512, 480, "Jaga Monster");
+            Raylib.SetTargetFPS(60);
+
+            // Skapa objekt
+            Figur hero = new Figur("hero.png", 100, 100);
+            Figur monster = new Figur("monster.png", 100, 200);
+            Figur hus = new Figur("house.png", 300, 350);
+            Figur damm = new Figur("pond.png", 400, 100);
+
+            // Speloopen
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.DARKGREEN);
+
+                // Rita ut här
+                hero.Rita();
+                monster.Rita();
+                hus.Rita();
+                damm.Rita();
+
+                Raylib.EndDrawing();
+            }
+        }
+    }
+}
