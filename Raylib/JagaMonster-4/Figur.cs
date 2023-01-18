6dabc08e06b72a06be566179d501e70a2e73c21b
karye <karye.webb@gmail.com>
2023-01-18 11:02:10 +0100

    Förflyttningar

---
+++ Raylib/JagaMonster-4/Figur.cs
@@ -0,0 +1,61 @@
+using System;
+using Raylib_cs;
+
+namespace JagaMonster_4
+{
+    public class Figur
+    {
+        int _x = 100;
+        int _y = 100;
+        int _vx = 1;
+        int _vy = 1;
+        Texture2D _textur;
+
+        // Konstruktor nr 1 (ctor)
+        public Figur(string bild, int x, int y)
+        {
+            _textur = Raylib.LoadTexture("./resurser/" + bild);
+            _x = x;
+            _y = y;
+        }
+
+        // Konstruktor nr 2 med slumpad startposition (ctor)
+        public Figur(string bild)
+        {
+            _textur = Raylib.LoadTexture("./resurser/" + bild);
+            _x = Raylib.GetRandomValue(32, 512-30-32);
+            _y = Raylib.GetRandomValue(32, 480-32-32);
+            _vx = 0;
+            _vy = Raylib.GetRandomValue(1, 10);
+        }
+
+        // Rita ut metoden (func)
+        public void Rita()
+        {
+            // Rita ut bilden
+            Raylib.DrawTexture(_textur, _x, _y, Color.WHITE);
+        }
+
+        // Ändra position (func)
+        public void Update()
+        {
+            // Horisontell
+            _x += _vx;
+
+            // Om figuren når högerkanten börja om
+            if (_x > 512-30-32)
+            {
+                _x = 30;
+            }
+
+            // Vertikal
+            _y += _vy;
+
+            // Om figuren når nederkanten börja om
+            if (_y > 480-32-32)
+            {
+                _y = 0;
+            }
+        }
+    }
+}
---
+++ Raylib/JagaMonster-4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Raylib_cs;
+
+namespace JagaMonster_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(512, 480, "Raylib");
+            Raylib.SetTargetFPS(60);
+            
+            // Skapa ett objekt
+            Figur hero = new Figur("hero.png", 100, 100);
+            Figur bakgrund = new Figur("background.png", 0, 0);
+
+            // Skapa 100 monster
+            List<Figur> listaMonster = new List<Figur>();
+            for (int i = 0; i < 100; i++)
+            {
+                listaMonster.Add(new Figur("monster.png"));
+            }
+            
+            while (!Raylib.WindowShouldClose())
+            {
+                /* Upddatera saker */
+                hero.Update();
+                foreach (var monster in listaMonster)
+                {
+                    monster.Update();
+                }
+
+                /* Rita ut saker */
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+
+                // Rita ut objektet
+                bakgrund.Rita();
+                hero.Rita();
+
+                // Rita ut 100 monster
+                foreach (var monster in listaMonster)
+                {
+                    monster.Rita();
+                }
+                
+                Raylib.EndDrawing();
+            }
+        }
+    }
+}
