af94b597be99ad2e5bca388ca330af7d420acf6e
karye <karye.webb@gmail.com>
2023-01-24 13:25:36 +0100

    Tränat på skapa klass, flytta med piltangenter

---
+++ Raylib/JagaMonster-5/Hjälte.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Numerics;
+using Raylib_cs;
+
+namespace JagaMonster_5
+{
+    public class Hjälte
+    {
+        int _x = 0;
+        int _y = 0;
+        Texture2D _textur;
+        Vector2 _förflyttning;
+
+        // Konstruktor (ctor)
+        public Hjälte(string bild)
+        {
+            _textur = Raylib.LoadTexture("./resurser/" + bild);
+        }
+
+        // Rita ut metoden (func)
+        public void Rita()
+        {
+            // Rita ut bilden
+            Raylib.DrawTexture(_textur, _x, _y, Color.WHITE);
+        }
+
+        // Ändra position mha piltangenterna
+        public void Uppdatera()
+        {
+            // Förflyttningsvektor
+            _förflyttning = new Vector2(0, 0);
+
+            // Lyssna på tangenter
+            // Om pil vänster/höger/upp/ned då flytta
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+            {
+                _förflyttning.X -= 1;
+            }
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+            {
+                _förflyttning.X += 1;
+            }
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
+            {
+                _förflyttning.Y -= 1;
+            }
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
+            {
+                _förflyttning.Y += 1;
+            }
+
+            // Flytta!
+            _x += (int)_förflyttning.X;
+            _y += (int)_förflyttning.Y;
+        }
+    }
+}
---
+++ Raylib/JagaMonster-5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Raylib_cs;
+
+namespace JagaMonster_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(512, 480, "Raylib");
+            Raylib.SetTargetFPS(60);
+
+            // Skapa bakgrunden
+            Figur bakgrund = new Figur("background.png", 0, 0);
+            
+            // Skapa ett monster mha klassen Figur
+            Figur monster = new Figur("monster.png");
+
+            // Skapa en hjälte
+            Hjälte hero = new Hjälte("hero.png");
+            
+            while (!Raylib.WindowShouldClose())
+            {
+                // Uppdatera objekten
+                hero.Uppdatera();
+
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.DARKGREEN);
+
+                // Rita ut bakgrunden
+                bakgrund.Rita();
+
+                // Rita ut monstret
+                monster.Rita();
+
+                // Rita ut hjälten
+                hero.Rita();
+                
+                Raylib.EndDrawing();
+            }
+        }
+    }
+}
