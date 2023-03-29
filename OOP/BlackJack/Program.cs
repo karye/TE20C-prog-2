7c826b6a69b8e1487fb0156d81dca435341dd609
karye <karye.webb@gmail.com>
2023-03-29 11:06:44 +0200

    Ett steg till i spelets flöde

--- OOP/BlackJack/Program.cs
+++ OOP/BlackJack/Program.cs
@@ -7,7 +7,11 @@ namespace BlackJack
     {
         static void Main(string[] args)
         {
+            Console.Clear();
             Console.WriteLine("Välkommen till Black Jack!");
+
+            // Spelstatus
+            string spelStatus = "spelar";
 
             // Skapa spelaren och dealern
             Console.Write("Vad heter du? ");
@@ -29,9 +33,11 @@ namespace BlackJack
             spelare1.TaEmotKort(kort2);
 
             Console.WriteLine("Du får två kort: ");
-            Console.WriteLine("-" + kort1.TillText());
-            Console.WriteLine("-"+ kort2.TillText());
+            Console.WriteLine("- " + kort1.TillText());
+            Console.WriteLine("- "+ kort2.TillText());
             Console.WriteLine("Din poängsumma är " + spelare1.Poäng());
+
+            // Fick spelaren BlackJakck?
 
             // .. och två kort till dealern
             // Ett av dealerns kort ska vara öppet
@@ -40,11 +46,48 @@ namespace BlackJack
             dealer.TaEmotKort(kort1);
             dealer.TaEmotKort(kort2);
 
+            // Fick dealern BlackJack?
+
             Console.WriteLine("Dealerns första kort är:");
-            Console.WriteLine("-" + kort1.TillText());
+            Console.WriteLine("- " + kort1.TillText());
 
             // Spelaren får välja att dra kort eller stanna. Om spelaren stannar, går turen vidare till dealern.
-            
+            while (true)
+            {
+                Console.Write("Vill du dra ett kort till? (j/n)");
+                if (Console.ReadLine().ToLower() == "j")
+                {
+                    // Ge spelaren ett till kort
+                    kort1 = kortlek.DraKort();
+                    spelare1.TaEmotKort(kort1);
+
+                    // Skriv ut alla kort i spelarens hand
+                    Console.WriteLine("Din hand är:");
+                    foreach (var kort in spelare1.Hand())
+                    {
+                        Console.WriteLine("- " + kort.TillText());
+                    }
+                    Console.WriteLine("Din poängsumma är " + spelare1.Poäng());
+
+                    // Om spelaren får 21, vinner spelaren direkt
+                    if (spelare1.Poäng() == 21)
+                    {
+                        Console.WriteLine("Du vann! Du fick Black Jack!");
+                        spelStatus = "vunnit";
+                        break;
+                    }
+                    else if (spelare1.Poäng() > 21)
+                    {
+                        Console.WriteLine("Du förlorade! Du fick mer än 21 poäng!");
+                        spelStatus = "förlorat";
+                        break;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
         }
     }
 }
